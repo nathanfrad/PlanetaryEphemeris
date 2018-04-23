80104f1b4d046e05586866bb14ffa06fbda6d91d
Nathan Fradet <nathanfrad86@gmail.com>
2018-04-23 09:30:03 +0200

    main

--- src/Calcul/Main.java
+++ src/Calcul/Main.java
@@ -7,10 +7,17 @@ public class Main {
 		double longitude_Ville = 0.340375; //Longitude geographique de Poitiers en degres decimaux
 		double latitude_Ville = 46.580224; // Latitude geographique de Poitiers en degres decimaux
 		double hauteur_Ville = 65; // Hauteur(altitude) de poitiers en mètres
-		String demande = "Moon";
+		String demande = "Venus";
 		LocalPosition local = new LocalPosition(demande, longitude_Ville, latitude_Ville, hauteur_Ville);
 		// GreenwichPosition local = new GreenwichPosition(demande);
 
+		//Alzimute alz  = new Alzimute(demande, longitude_Ville, latitude_Ville, hauteur_Ville);
 	}
+
+
+
+
+
+
 
 }
